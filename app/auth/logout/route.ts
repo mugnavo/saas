4601a49d15b107badc0e2f51bd6f691d639b2947
Nathaniel Tampus <nate@mugnavo.com>
2024-03-30 23:38:37 +0800

    add logout handler

---
+++ app/auth/logout/route.ts
@@ -0,0 +1,27 @@
+import { cookies } from "next/headers";
+
+import { lucia, validateRequest } from "~/lib/auth";
+
+export async function GET() {
+  const { session } = await validateRequest();
+  if (!session) {
+    return new Response(null, {
+      status: 401,
+      headers: {
+        Location: "/",
+      },
+    });
+  }
+
+  await lucia.invalidateSession(session.id);
+
+  const sessionCookie = lucia.createBlankSessionCookie();
+  cookies().set(sessionCookie.name, sessionCookie.value, sessionCookie.attributes);
+
+  return new Response(null, {
+    status: 302,
+    headers: {
+      Location: "/",
+    },
+  });
+}
